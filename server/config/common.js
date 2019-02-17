e752460ab4c2821f90b4cf3edfd97862ad3cb6d3
Farolan Faisal <farolanfaisal@gmail.com>
2019-02-17 08:56:45 +0800

    white list resources

--- server/config/common.js
+++ server/config/common.js
@@ -39,12 +39,29 @@ module.exports = {
           { path: '/auth/register', method: 'POST' },
           { path: '/auth/logout', method: 'GET' },
           { path: '/auth/verify', method: 'GET' },
-          { path: '/auth/unique-email', method: 'POST' },
-          { path: '/Departments', method: 'GET' },
+          { path: '/auth/unique-email', method: 'GET' },
+
+          // public resources
+          { path: '/Departments(/.*)?', method: 'GET' },
+          { path: '/Categories(/.*)?', method: 'GET' },
+          { path: '/ProductCategories(/.*)?', method: 'GET' },
+          { path: '/Products(/.*)?', method: 'GET' },
+          { path: '/ProductAttributes(/.*)?', method: 'GET' },
+          { path: '/Attributes(/.*)?', method: 'GET' },
+          { path: '/AttributeValues(/.*)?', method: 'GET' },
+          { path: '/Reviews(/.*)?', method: 'GET' },
+          { path: '/Taxes(/.*)?', method: 'GET' },
+          { path: '/Shippings(/.*)?', method: 'GET' },
+          { path: '/ShippingRegions(/.*)?', method: 'GET' },
         ]
       },
       customer: {
         routes: [
+          // protected resources - only owner can access
+          { path: '/Customers/\\d+', method: ['GET', 'PUT'] },
+          { path: '/Orders/\\d+', method: ['GET', 'PUT'] },
+          { path: '/OrderDetails/\\d+', method: ['GET', 'PUT'] },
+          { path: '/ShoppingCarts/\\d+', method: ['GET', 'PUT'] },
         ]
       },
       // admin passed all checks
