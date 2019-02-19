6e28b31315df26b2c856ce050dea22025fbed208
Farolan Faisal <farolanfaisal@gmail.com>
2019-02-19 13:39:49 +0800

    fix prop leaking

--- frontend/src/lib/react-admin/image-field.js
+++ frontend/src/lib/react-admin/image-field.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { API_HOST } from '$src/const'
 
 // eslint-disable-next-line
-const ImageField = ({ source, record = {}, sortable, basePath, ...props }) => (
-  <img src={API_HOST + `/${record[source]}`} {...props} />
+const ImageField = ({ source, record = {}, style }) => (
+  <img src={API_HOST + `/${record[source]}`} style={style} />
 )
 
 export default ImageField
