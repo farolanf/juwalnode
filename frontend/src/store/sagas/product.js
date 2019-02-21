46075b88140438bde644ca4c141f086ead400a68
Farolan Faisal <farolanfaisal@gmail.com>
2019-02-21 13:34:09 +0800

    correct case

--- frontend/src/store/sagas/product.js
+++ frontend/src/store/sagas/product.js
@@ -1,4 +1,6 @@
 import axios from 'axios'
+import _ from 'lodash'
+import changeCase from 'change-case'
 
 import { API_BASE } from '$src/const'
 import { fetchActionSaga } from '$src/lib/action'
@@ -7,10 +9,10 @@ import { fetchProducts } from '$act/product'
 function fetchApi ({ payload: { department, category, attributes, page }}) {
   const params = { count: 15, page }
   if (department) {
-    params.departments = department
+    params.departments = _.castArray(department).map(changeCase.sentenceCase)
   }
   if (category) {
-    params.categories = category
+    params.categories = _.castArray(category).map(changeCase.sentenceCase)
   }
   if (attributes) {
     params.attributes = JSON.stringify(attributes)
