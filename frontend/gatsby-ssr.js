61f3f4bdfa6e86812fd3b2f570c74e77809a4c16
Farolan Faisal <farolanfaisal@gmail.com>
2019-02-23 21:37:43 +0800

    add deploy script

--- frontend/gatsby-ssr.js
+++ frontend/gatsby-ssr.js
@@ -7,11 +7,13 @@ import 'typeface-roboto'
 import _wrapRootElement from './wrapRootElement'
 import { verify } from '$src/lib/auth'
 
+const config = require('./gatsby-config')
+
 verify()
 
 export const wrapPageElement = ({ element }) => {
-   // no ssr for /admin
-   if (element.props.location.pathname.startsWith('/admin/')) {
+  // no ssr for /admin
+  if (element.props.location.pathname.startsWith((config.pathPrefix || '') + '/admin/')) {
     return null
   }
   return _wrapRootElement(element)
