0372db3232e85958453049848c7c5d6834c68df5
Farolan Faisal <farolanfaisal@gmail.com>
2019-02-24 11:07:38 +0800

    fix admin pathname check

--- frontend/wrapRootElement/index.js
+++ frontend/wrapRootElement/index.js
@@ -2,9 +2,11 @@ import { compose } from 'lodash/fp'
 import redux from './redux'
 import mui from './mui'
 
+import config from '../gatsby-config'
+
 export default element => {
   // don't wrap react-admin
-  if (element.props.location.pathname.startsWith('/admin/')) {
+  if (element.props.location.pathname.startsWith((config.pathPrefix || '') + '/admin/')) {
     return element
   }
   return compose(
