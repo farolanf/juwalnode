026496a3717936125c0fb328e98e7e9a73afb9b4
Farolan Faisal <farolanfaisal@gmail.com>
2019-02-27 07:39:54 +0800

    update ShoppingCart model

--- server/sequelize/models/shoppingcart.js
+++ server/sequelize/models/shoppingcart.js
@@ -9,10 +9,10 @@ module.exports = (sequelize, DataTypes) => {
     },
     cart_id: { type: DataTypes.CHAR(32), allowNull: false },
     product_id: { type: DataTypes.INTEGER, allowNull: false },
+    // sequelize error when using 'attributes' field name, so use 'attrs' instead
     attrs: { type: DataTypes.STRING(1000), allowNull: false, field: 'attributes' },
     quantity: { type: DataTypes.INTEGER, allowNull: false },
-    buy_now: { type: DataTypes.TINYINT(1), allowNull: false },
-    added_on: { type: DataTypes.DATE, allowNull: false }
+    buy_now: { type: DataTypes.TINYINT(1), allowNull: false, defaultValue: true }
   }, {
     tableName: 'shopping_cart',
     createdAt: 'added_on',
@@ -20,6 +20,7 @@ module.exports = (sequelize, DataTypes) => {
   });
   ShoppingCart.associate = function(models) {
     ShoppingCart.belongsTo(models.Product, { foreignKey: 'product_id' })
+    ShoppingCart.hasOne(models.OrderDetail, { foreignKey: 'item_id' })
   };
   return ShoppingCart;
 };
