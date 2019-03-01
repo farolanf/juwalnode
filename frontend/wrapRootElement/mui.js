f5a0c6103406184ab5b614d13c578467cb047731
Farolan Faisal <farolanfaisal@gmail.com>
2019-03-01 09:07:47 +0800

    use custom generateClassName

--- frontend/wrapRootElement/mui.js
+++ frontend/wrapRootElement/mui.js
@@ -1,13 +1,20 @@
 import React from 'react'
-import { MuiThemeProvider } from '@material-ui/core/styles'
+import JssProvider from 'react-jss/lib/JssProvider'
+import { MuiThemeProvider, createGenerateClassName } from '@material-ui/core/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import theme from '$src/theme'
 
+const generateClassName = createGenerateClassName({
+  seed: 'e9de8ee1'
+})
+
 const wrapRootElement = element => (
-  <MuiThemeProvider theme={theme}>
-    <CssBaseline />
-    {element}
-  </MuiThemeProvider>
+  <JssProvider generateClassName={generateClassName}>
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      {element}
+    </MuiThemeProvider>
+  </JssProvider>
 )
 
 export default wrapRootElement
