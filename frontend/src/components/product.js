986d95eb2f54c098b05b41957d04ded57db7e90d
Farolan Faisal <farolanfaisal@gmail.com>
2019-03-03 15:45:03 +0800

    remove add to cart on browse

--- frontend/src/components/product.js
+++ frontend/src/components/product.js
@@ -6,32 +6,22 @@ import Card from '@material-ui/core/Card'
 import CardActionArea from '@material-ui/core/CardActionArea'
 import CardMedia from '@material-ui/core/CardMedia'
 import CardContent from '@material-ui/core/CardContent'
-import CardActions from '@material-ui/core/CardActions'
-import Button from '@material-ui/core/Button'
 
 import { API_HOST } from '$src/const'
+import { formatCurrency } from '$lib/format'
 
 const styles = () => ({
-  root: tw`flex flex-col w-full items-center pt-4 pb-3`,
+  root: tw`flex flex-col w-full items-center pt-4`,
   media: tw`m-auto xs:h-64 md:h-48 bg-contain`,
-  content: tw`text-center pt-4 pb-2`,
-  title: tw`text-base font-bold`,
+  content: tw`text-center`,
+  title: tw`text-base font-bold mb-4`,
+  price: tw`text-base mb-4`,
   actions: tw`justify-center flex-grow`,
 })
 
-const Product = ({ classes, item, addCartItem }) => {
+const Product = ({ classes, item }) => {
   function handleViewDetail () {
     navigate('/products/' + item._id)
-  }
-  function handleClickAddToCart () {
-    addCartItem({
-      product_id: item._source.product_id,
-      attrs: [
-        { name: 'Color', value: 'Orange' },
-        { name: 'Size', value: 'M' },
-      ],
-      quantity: 1,
-    })
   }
   return (
     <Card className={classes.root}>
@@ -41,18 +31,11 @@ const Product = ({ classes, item, addCartItem }) => {
           <div className={classes.title}>
             {item._source.name}
           </div>
+          <div className={classes.price}>
+            {formatCurrency(item._source.price)}
+          </div>
         </CardContent>
       </CardActionArea>
-      <CardActions className={classes.actions}>
-        <Button
-          variant='outlined'
-          size='small'
-          color='primary'
-          onClick={handleClickAddToCart}
-        >
-          Add to cart
-        </Button>
-      </CardActions>
     </Card>
   )
 }
