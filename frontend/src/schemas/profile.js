bc015731c83ce8ce3e97bf2d19c5edb5a0736c39
Farolan Faisal <farolanfaisal@gmail.com>
2019-03-05 17:29:36 +0800

    allow null profile props

--- frontend/src/schemas/profile.js
+++ frontend/src/schemas/profile.js
@@ -2,14 +2,14 @@ import * as Yup from 'yup'
 
 export default  Yup.object().shape({
   credit_card: Yup.string().max(100).nullable(true),
-  address_1: Yup.string().max(100),
-  address_2: Yup.string().max(100),
-  city: Yup.string().max(100),
-  region: Yup.string().max(100),
-  postal_code: Yup.string().max(100),
-  country: Yup.string().max(100),
+  address_1: Yup.string().max(100).nullable(true),
+  address_2: Yup.string().max(100).nullable(true),
+  city: Yup.string().max(100).nullable(true),
+  region: Yup.string().max(100).nullable(true),
+  postal_code: Yup.string().max(100).nullable(true),
+  country: Yup.string().max(100).nullable(true),
   shipping_region_id: Yup.number(),
-  day_phone: Yup.string().max(100),
-  eve_phone: Yup.string().max(100),
-  mob_phone: Yup.string().max(100),
+  day_phone: Yup.string().max(100).nullable(true),
+  eve_phone: Yup.string().max(100).nullable(true),
+  mob_phone: Yup.string().max(100).nullable(true),
 })
