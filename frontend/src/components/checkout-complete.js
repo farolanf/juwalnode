5f44a4b2f9e518aff3713271d8ab4c2141268f58
Farolan Faisal <farolanfaisal@gmail.com>
2019-03-07 09:19:41 +0800

    update checkout comlete page

--- frontend/src/components/checkout-complete.js
+++ frontend/src/components/checkout-complete.js
@@ -1,12 +1,19 @@
 import React, { useEffect } from 'react'
+import Alert from '$comp/alert'
 
-const CheckoutComplete = ({ fetchCart }) => {
+import withStyles from '@material-ui/core/styles/withStyles'
+
+const styles = () => ({
+  root: tw`my-8 mx-2`,
+})
+
+const CheckoutComplete = ({ fetchCart, classes }) => {
   useEffect(() => {
     fetchCart()
   }, [])
   return (
-    <div>Thank you. Your order will be processed and your order summary will be emailed to you shortly.</div>
+    <Alert variant='success' message='Thank you. Your order will be processed and your order summary will be emailed to you shortly.' className={classes.root} />
   )
 }
 
-export default CheckoutComplete
+export default withStyles(styles)(CheckoutComplete)
