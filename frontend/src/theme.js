fd2066e5ba589b5e3be73f45c3f77fefe3ef2774
Farolan Faisal <farolanfaisal@gmail.com>
2019-03-07 09:19:34 +0800

    add alert colors

--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -1,11 +1,18 @@
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
 import indigo from '@material-ui/core/colors/indigo'
 import blue from '@material-ui/core/colors/blue'
+import red from '@material-ui/core/colors/red'
+import green from '@material-ui/core/colors/green'
+import amber from '@material-ui/core/colors/amber'
 
 const theme = createMuiTheme({
   palette: {
     primary: indigo,
     secondary: blue,
+    error: red,
+    info: blue,
+    success: green,
+    warning: amber,
   },
   typography: {
     useNextVariants: true
