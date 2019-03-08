41bd0d69af186f9ab2d718c3c749a3248c7c1287
Farolan Faisal <farolanfaisal@gmail.com>
2019-03-08 18:43:26 +0800

    fix command hanging

--- cmd.js
+++ cmd.js
@@ -4,7 +4,10 @@ global.app = require('./server/app')
 
 if (process.argv.length < 3) {
   cmd.print()
+  process.exit()
 } else {
   const fullName = process.argv[2]
-  cmd.run(fullName).then(() => process.exit())
+  cmd.run(fullName)
+    .catch(console.log)
+    .finally(() => process.exit())
 }
